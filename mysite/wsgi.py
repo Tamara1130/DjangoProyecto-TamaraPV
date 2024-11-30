a147bc3b940d0ce091f6094abda136014249a32b
Tamara1130 <tamara.pena36@my.unitec.edu.mx>
2024-11-30 14:31:26 -0600

    wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,18 @@
 """
-WSGI config for mysite project.
+Configuración de WSGI para el proyecto mysite.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+Este archivo expone la aplicación WSGI como una variable a nivel de módulo llamada application.
 """
 
+# Importamos la funcion "os" para interactuar con el sistema operativo
 import os
 
+# Importamos la función "get_wsgi_application" para configurar la aplicación WSGI
 from django.core.wsgi import get_wsgi_application
 
+# Configuramos el archivo de configuración del proyecto (asegura que se use el archivo "settings.py" correctamente).
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+# Creamos la instancia WSGI (usada para manejar las solicitudes).
 application = get_wsgi_application()
+
